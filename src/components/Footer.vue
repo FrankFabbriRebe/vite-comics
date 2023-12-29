36f4ae0516b9ce14de39ac9ec2537a8ac20cba74
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-12-29 15:33:35 +0100

    add: content in footer components

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,55 @@ export default {
 </script>
 
 <template>
-    <h1>Footer</h1>
+    <footer>
+        <div class="container">
+
+            <div class="sing_up">
+                <button>SING-UP NOW!</button>
+            </div>
+
+            <div class="follow_us">
+                <h3>FOLLOW US</h3>
+                <a href="#">
+                    <img src="/imgs/footer-facebook.png" alt="facebbok">
+                </a>
+                <a href="#">
+                    <img src="/imgs/footer-youtube.png" alt="youtube">
+                </a>
+                <a href="#">
+                    <img src="/imgs/footer-twitter.png" alt="twitter">
+                </a>
+            </div>
+
+        </div>
+    </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+footer {
+    background-color: #303030;
+}
+
+.container {
+    padding: 10px 50px;
+    display: flex;
+    justify-content: space-between;
+}
+
+button {
+    padding: 5px;
+    border-color: #0582F9;
+    background-color: #303030;
+    color: white;
+}
+
+.follow_us {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+}
+
+h3 {
+    color: #0582F9;
+}
+</style>
