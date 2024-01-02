1e91d35a0489874867923607cd549812425512c2
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-02 17:04:33 +0100

    add: jumbotron

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,8 +6,12 @@ export default {
 
 <template>
     <main>
+        <div class="jumbotron">
+
+        </div>
         <div class="container">
-            <span>Content goes here</span>
+
+
         </div>
     </main>
 </template>
@@ -19,9 +23,17 @@ export default {
 main {
     background-color: black;
     color: white;
+
 }
 
 .container {
     @include container;
+}
+
+.jumbotron {
+    background-image: url('/imgs/jumbotron.jpg');
+    background-size: cover;
+    width: 100%;
+    height: 300px;
 }
 </style>
