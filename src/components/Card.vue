9971799c9cb0940ea701cc225e7ba430e7942047
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-02 18:19:25 +0100

    add: content using props

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,38 @@
 <script>
 export default {
-    name: "Card"
+    name: "Card",
+    props: {
+        details: Object
+    }
 }
 </script>
 
-<template></template>
+<template>
+    <div class="card">
+        <div class="cover">
+            <img :src="details.thumb" alt="details.series">
+        </div>
+        <h3>{{ details.series }}</h3>
+    </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+    width: calc(100% / 6 - 10px);
+    margin-bottom: 30px;
+    margin-top: 30px;
+
+    .cover {
+        background-color: green;
+        width: 100%;
+        height: 250px;
+        margin-bottom: 10px;
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+}
+</style>
